dbe0cfec502952b269d8e31278814ff46a660003
Naoufal Temmar <naoufal450@gmail.com>
2020-04-21 22:44:27 +0000

    TokenSDK Dependencies.

--- contracts/src/main/java/com/octo/utils/Require.java
+++
@@ -1,7 +0,0 @@
-package com.octo.utils;
-
-public class Require {
-    public static void using(String message, Boolean expression) {
-        if (!expression) throw new IllegalArgumentException("Failed requirement: " + message);
-    }
-}
